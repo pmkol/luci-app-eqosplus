3ecdb23f93a7396fbb509e489b2ccf12ceb1f059
MapleCool <37971849+pmkol@users.noreply.github.com>
2024-10-07 00:52:33 +0800

    Update update-feeds.sh

--- update-feeds.sh
+++ update-feeds.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #
-curl -s https://downloads.openwrt.org/releases/packages-23.05/x86_64/packages/Packages | grep '^Package:' | awk '{print $2}' | sort > openwrt-packages
-curl -s https://downloads.immortalwrt.org/releases/packages-23.05/x86_64/packages/Packages | grep '^Package:' | awk '{print $2}' | sort > immortalwrt-packages
+arch=$1
+echo "matrix.arch = $arch"
+[[ "$arch" != "x86" && "$arch" != "aarch64_generic" ]] && echo "Unsupported architecture: $arch. Exiting..." && exit 1
+
+curl -s https://downloads.openwrt.org/releases/packages-23.05/$arch/packages/Packages | grep '^Package:' | awk '{print $2}' | sort > openwrt-packages
+curl -s https://downloads.immortalwrt.org/releases/packages-23.05/$arch/packages/Packages | grep '^Package:' | awk '{print $2}' | sort > immortalwrt-packages
+curl -s https://downloads.openwrt.org/releases/packages-23.05/$arch/luci/Packages | grep '^Package:' | awk '{print $2}' | sort > openwrt-packages
+curl -s https://downloads.immortalwrt.org/releases/packages-23.05/$arch/luc/Packages | grep '^Package:' | awk '{print $2}' | sort > immortalwrt-packages
 grep -Fxf openwrt-packages immortalwrt-packages > remove-packages
+grep -Fxf openwrt-packages immortalwrt-packages >> remove-packages
 
 git clone https://github.com/immortalwrt/packages immortalwrt/packages --depth 1
 git clone https://github.com/immortalwrt/luci immortalwrt/luci --depth 1
