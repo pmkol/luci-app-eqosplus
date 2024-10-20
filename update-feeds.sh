0d90c6520337ec5e8323488e82e08ff8f7f31d44
MapleCool <37971849+pmkol@users.noreply.github.com>
2024-10-21 02:34:34 +0800

    Update update-feeds.sh

--- update-feeds.sh
+++ update-feeds.sh
@@ -7,11 +7,12 @@ mv */ /tmp/bak/
 
 # download feeds
 git clone https://github.com/openwrt/luci openwrt/luci -b openwrt-23.05 --depth 1
-git clone https://github.com/openwrt/packages openwrt/packages -b master --depth 1
-git clone https://github.com/immortalwrt/luci immortalwrt/luci -b openwrt-23.05 --depth 1
-git clone https://github.com/immortalwrt/packages immortalwrt/packages -b openwrt-23.05 --depth 1
+git clone https://github.com/openwrt/packages openwrt/packages -b openwrt-23.05 --depth 1
+git clone https://github.com/immortalwrt/luci immortalwrt/luci -b master --depth 1
+git clone https://github.com/immortalwrt/packages immortalwrt/packages -b master --depth 1
 git clone https://github.com/immortalwrt/homeproxy immortalwrt/luci-app-homeproxy --depth 1
 git clone https://github.com/sirpdboy/luci-app-ddns-go openwrt-ddns-go --depth 1
+git clone https://github.com/QiuSimons/luci-app-daed openwrt-daed --depth 1
 git clone https://github.com/sbwml/openwrt_pkgs --depth 1
 git clone https://github.com/sbwml/openwrt_helloworld --depth 1
 git clone https://github.com/sbwml/luci-app-alist openwrt-alist --depth 1
@@ -19,62 +20,94 @@ git clone https://github.com/sbwml/luci-app-airconnect openwrt-airconnect --dept
 git clone https://github.com/sbwml/luci-app-mentohust openwrt-mentohust --depth 1
 git clone https://github.com/sbwml/luci-app-mosdns openwrt-mosdns --depth 1
 git clone https://github.com/sbwml/luci-app-qbittorrent openwrt-qbittorrent --depth 1
+git clone https://github.com/sbwml/luci-theme-argon openwrt-argon --depth 1
 git clone https://github.com/sbwml/packages_utils_containerd --depth 1
-#git clone https://github.com/sbwml/packages_utils_docker --depth 1
+git clone https://github.com/sbwml/packages_utils_docker --depth 1
 git clone https://github.com/sbwml/packages_utils_dockerd --depth 1
 git clone https://github.com/sbwml/packages_utils_runc --depth 1
-#git clone https://github.com/pmkol/openwrt-eqosplus --depth 1
+git clone https://github.com/sbwml/OpenAppFilter openwrt-oaf --depth 1
+git clone https://github.com/sbwml/feeds_packages_libs_liburing --depth 1
+git clone https://github.com/sbwml/feeds_packages_net_samba4 --depth 1
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic --depth 1
+git clone https://github.com/hudra0/qosmate --depth 1
+git clone https://github.com/pmkol/luci-app-qosmate --depth 1
+git clone https://github.com/pmkol/openwrt-eqosplus --depth 1
 git clone https://github.com/pmkol/openwrt-mihomo --depth 1
+git clone https://github.com/pmkol/v2ray-geodata --depth 1
+git clone https://github.com/pmkol/packages_net_miniupnpd miniupnpd --depth 1
+git clone https://github.com/pmkol/luci-app-upnp --depth 1
 
 rm -rf immortalwrt/luci-app-homeproxy/{LICENSE,README}
 rm -rf openwrt-ddns-go/luci-app-ddns-go/README.md
-rm -rf openwrt_pkgs/fw_download_tool
-rm -rf openwrt_pkgs/lshw
-rm -rf openwrt_pkgs/luci-app-eqos wget patch
-rm -rf openwrt_pkgs/luci-app-ota
-rm -rf openwrt_pkgs/luci-app-zerotier wget patch
-rm -rf openwrt_pkgs/rtl_fm_streamer
-rm -rf openwrt_helloworld/
+rm -rf openwrt_pkgs/{fw_download_tool,lshw,luci-app-eqos,luci-app-ota,luci-app-zerotier,rtl_fm_streamer}
+rm -rf openwrt_helloworld/{luci-app-homeproxy,luci-app-mihomo,mihomo,v2ray-geodata}
+#rm -rf openwrt-alist/alist
+rm -rf feeds_packages_libs_liburing/.git
+rm -rf feeds_packages_net_samba4/.git
+rm -rf packages_utils_containerd/.git
+rm -rf packages_utils_docker/.git
+rm -rf packages_utils_dockerd/.git
+rm -rf packages_utils_runc/.git
+rm -rf luci-app-unblockneteasemusic/.git
+rm -rf qosmate/.git
+rm -rf luci-app-qosmate/.git
+rm -rf v2ray-geodata/.git
+rm -rf packages_net_miniupnpd/.git
+rm -rf luci-app-upnp/.git
 
 # download patch
 mv -f openwrt_helloworld/*.patch ./
-curl https://raw.githubusercontent.com/pmkol/openwrt-plus/refs/heads/master/openwrt/patch/docker/0001-dockerd-fix-bridge-network.patch > patch_docker_1.patch
-curl https://raw.githubusercontent.com/pmkol/openwrt-plus/refs/heads/master/openwrt/patch/docker/0002-docker-add-buildkit-experimental-support.patch patch_docker_2.patch
-curl https://github.com/pmkol/openwrt-plus/raw/refs/heads/master/openwrt/patch/docker/0003-dockerd-disable-ip6tables-for-bridge-network-by-defa.patch > patch_docker_3.patch
+curl -s https://raw.githubusercontent.com/pmkol/openwrt-plus/master/openwrt/patch/docker/0001-dockerd-fix-bridge-network.patch > patch-dockerd-fix-bridge-network.patch
+curl -s https://raw.githubusercontent.com/pmkol/openwrt-plus/master/openwrt/patch/docker/0002-docker-add-buildkit-experimental-support.patch patch-docker-add-buildkit-experimental-support.patch
+curl -s https://raw.githubusercontent.com/pmkol/openwrt-plus/master/openwrt/patch/docker/0003-dockerd-disable-ip6tables-for-bridge-network-by-defa.patch > patch-dockerd-disable-ip6tables-for-bridge-network-by-defa.patch
+curl -s https://raw.githubusercontent.com/pmkol/openwrt-plus/master/openwrt/patch/luci/applications/luci-app-frpc/001-luci-app-frpc-hide-token-openwrt-23.05.patch > patch-luci-app-frpc-hide-token.patch
+curl -s https://raw.githubusercontent.com/pmkol/openwrt-plus/master/openwrt/patch/luci/applications/luci-app-frpc/002-luci-app-frpc-add-enable-flag-openwrt-23.05.patch > patch-luci-app-frpc-add-enable-flag.patch
 
-mv immortalwrt/packages/utils/docker ./
+# add pkgs
+mv openwrt_pkgs/*/ ./
+rm -rf openwrt_pkgs
 
-# luci-app-alist
+# add helloworld
+mv openwrt_helloworld/*/ ./
+rm -rf openwrt_helloworld
+
+# add luci-app-alist
 mv openwrt-alist/*/ ./
 rm -rf openwrt-alist
 
-# luci-app-airconnect/*/ ./
-mv openwrt-airconnect
+# add luci-app-airconnect/*/ ./
+mv openwrt-airconnect/*/ ./
 rm -rf openwrt-airconnect
 
-# luci-app-ddns-go
+# add luci-theme-argon
+mv openwrt-argon/*/ ./
+rm -rf openwrt-argon
+
+# add luci-app-ddns-go
 mv openwrt-ddns-go/*/ ./
 rm -rf openwrt-ddns-go
 
-# luci-app-eqos
-#patch -p1 -f -s -d immortalwrt/luci/applications < patch-luci-app-eqos.patch
-#if [ $? -eq 0 ]; then
-#    rm -rf luci-app-eqos
-#    mv immortalwrt/luci/applications/luci-app-eqos luci-app-eqos
-#else
-#    mv /tmp/bak/luci-app-eqos luci-app-eqos
-#fi
-
-# luci-app-eqosplus
-#mv openwrt-eqosplus/*/ ./
+#add luci-app-eqosplus
+mv openwrt-eqosplus/*/ ./
+rm -rf openwrt-eqosplus
 
 # luci-app-frpc
 mv openwrt/luci/applications/luci-app-frpc ./
 sed -i 's|../../luci.mk|$(TOPDIR)/feeds/luci/luci.mk|' luci-app-frpc/Makefile
-luci_frpc_commit_date=$(curl -s "https://api.github.com/repos/immortalwrt/luci/commits?path=applications/luci-app-frpc&sha=openwrt-23.05" | jq -r '.[0].commit.committer.date' | cut -d'T' -f1 | tr -d '-')
-sed -i "/PKG_MAINTAINER/i\PKG_PO_VERSION:=$luci_frpc_commit_date" luci-app-frpc/Makefile
-sed -i "/PKG_MAINTAINER/i\PKG_VERSION:=$luci_frpc_commit_date" luci-app-frpc/Makefile
-rm -rf luci-app-frpc/po/!(templates|zh_Hans|zh_Hant)
+sed -i 's,frp 客户端,FRP 客户端,g' luci-app-frpc/po/zh_Hans/frpc.po
+rm -rf luci-app-frpc/po/!(templates|zh_Hans)
+patch -p1 -f -s patch-luci-app-frpc-hide-token.patch
+patch -p1 -f -s patch-luci-app-frpc-add-enable-flag.patch
+mv openwrt/packages/net/frp ./
+sed -i 's/procd_set_param stdout $stdout/procd_set_param stdout 0/g' frp/files/frpc.init
+sed -i 's/procd_set_param stderr $stderr/procd_set_param stderr 0/g' frp/files/frpc.init
+sed -i 's/stdout stderr //g' frp/files/frpc.init
+sed -i '/stdout:bool/d;/stderr:bool/d' frp/files/frpc.init
+sed -i '/stdout/d;/stderr/d' frp/files/frpc.config
+sed -i 's/env conf_inc/env conf_inc enable/g' frp/files/frpc.init
+sed -i "s/'conf_inc:list(string)'/& \\\\/" frp/files/frpc.init
+sed -i "/conf_inc:list/a\\\t\t\'enable:bool:0\'" frp/files/frpc.init
+sed -i '/procd_open_instance/i\\t\[ "$enable" -ne 1 \] \&\& return 1\n' frp/files/frpc.init
 
 # luci-app-mentohust
 mv openwrt-mentohust/*/ ./
@@ -105,10 +138,41 @@ fi
 
 # luci-app-qbittorrent
 mv openwrt-qbittorrent/*/ ./
+rm -rf openwrt-qbittorrent
 
-# luci-app-zerotier
+# luci-app-ttyd
+sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+sed -i '3 a\\t\t"order": 50,' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+sed -i 's/procd_set_param stdout 1/procd_set_param stdout 0/g' feeds/packages/utils/ttyd/files/ttyd.init
+sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/utils/ttyd/files/ttyd.init
 
-mv immortalwrt/luci/applications/luci-app-zerotier luci-app-zerotier
+# smb
+# enable multi-channel
+sed -i '/workgroup/a \\n\t## enable multi-channel' feeds/packages/net/samba4/files/smb.conf.template
+sed -i '/enable multi-channel/a \\tserver multi channel support = yes' feeds/packages/net/samba4/files/smb.conf.template
+# default config
+sed -i 's/#aio read size = 0/aio read size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#aio write size = 0/aio write size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/bind interfaces only = yes/bind interfaces only = no/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#create mask/create mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#directory mask/directory mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/0666/0644/g;s/0744/0755/g;s/0777/0755/g' feeds/luci/applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4.js
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/samba.config
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.template
+
+
+# unblockneteasemusic
+sed -i 's/解除网易云音乐播放限制/网易云音乐解锁/g' luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
+
+# docker
+
+# nlbwmon
+mv openwrt/packages/net/nlbwmon ./
+sed -i 's/stderr 1/stderr 0/g' nlbwmon/files/nlbwmon.init
+mv openwrt/luci/applications/luci-app-nlbwmon ./
+sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
+sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
 
 # haproxy
 mv openwrt/packages/net/haproxy ./
